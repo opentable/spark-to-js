841561503d4df4523f35e76ef2eee8dc57430838
Matt Blair <mblair@opentable.com>
2015-01-13 11:40:20 -0800

    moving some assemblyInfo around for separate compiliation

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,8 +1,12 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("TemplateGen")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("OpenTable")]
+[assembly: AssemblyProduct("SearchResults")]
+[assembly: AssemblyCopyright("Copyright © OpenTable 2015")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0ff2cd11-0e06-4fdb-a766-2119039997a3")]
