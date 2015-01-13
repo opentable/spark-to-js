4e47184a828d9d5aa086c0715074cf69c899fbb8
Matt Blair <mblair@opentable.com>
2015-01-13 11:09:40 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Spark;
+using Spark.FileSystem;
+using Spark.Web.Mvc;
+
+namespace TemplateGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("You must supply the view folder path and output path.");
+					return;
+                }
+
+                var settings = new SparkSettings();
+
+                var factory = new SparkViewFactory(settings);
+
+                factory.ViewFolder = new FileSystemViewFolder(args[0]);
+
+                var files = Directory.GetFiles(args[0], "Shared/*.spark", SearchOption.AllDirectories);
+
+                foreach (string file in files)
+                {
+                    string parsedFile = file.Replace(string.Format("{0}\\Shared\\", args[0]), string.Empty);
+                    parsedFile = parsedFile.Replace(".spark", string.Empty);
+
+                    var buildDescriptorParams = new BuildDescriptorParams(string.Empty, string.Empty, parsedFile,
+                                                                          string.Empty, false,
+                                                                          new Dictionary<string, object>());
+
+                    var descriptor = factory.DescriptorBuilder.BuildDescriptor(buildDescriptorParams, new List<string>());
+                    descriptor.Language = LanguageType.Javascript;
+
+                    var entry = factory.Engine.CreateEntry(descriptor);
+
+                    string filePath = string.Format("{0}.js", Path.Combine(args[1], parsedFile));
+
+                    if (!Directory.Exists(Path.GetDirectoryName(filePath)))
+                    {
+                        Directory.CreateDirectory(Path.GetDirectoryName(filePath));
+                    }
+
+                    File.WriteAllText(filePath, entry.SourceCode);
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Exception: "+exception.Message);
+                Console.WriteLine(exception.StackTrace);
+                throw exception;
+            }
+            Console.WriteLine("Finished Template Generation.");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TemplateGen")]
+[assembly: AssemblyDescription("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0ff2cd11-0e06-4fdb-a766-2119039997a3")]
