b61e3bd0037bc19d4b0a5e31971857263eef8b7f
Matt Blair <duereg@hotmail.com>
2015-04-21 18:12:20 -0700

    add support for only generating changed templates

--- Program.cs
+++ Program.cs
@@ -26,6 +26,7 @@ namespace TemplateGenerator
                 var templates = new FileSystemViewFolder(args[0]);
                 var factory = new SparkViewFactory(settings) { ViewFolder = templates };
                 var files = Directory.GetFiles(args[0], "*.spark", SearchOption.AllDirectories);
+                var filesProcessed = 0;
 
                 Console.WriteLine("Found {0} files to process", files.Length);
 
@@ -33,23 +34,28 @@ namespace TemplateGenerator
                 {
                     string sparkFile = file.Replace(args[0] + System.IO.Path.DirectorySeparatorChar, string.Empty);
                     string fileName = sparkFile.Replace(".spark", string.Empty);
+                    string jsFilePath = string.Format("{0}.js", Path.Combine(args[1], fileName));
 
-                    var descriptor = new SparkViewDescriptor();
-                    descriptor.Language = LanguageType.Javascript;
-                    descriptor.Templates.Add(sparkFile);
-
-                    //Maybe this has to be done in order so the other templates exist?
-                    var entry = factory.Engine.CreateEntry(descriptor);
-
-                    string filePath = string.Format("{0}.js", Path.Combine(args[1], fileName));
-
-                    if (!Directory.Exists(Path.GetDirectoryName(filePath)))
+                    if (args.Length == 2 || (File.GetLastWriteTime(jsFilePath) < File.GetLastWriteTime(sparkFile)))
                     {
-                        Directory.CreateDirectory(Path.GetDirectoryName(filePath));
-                    }
+                        var descriptor = new SparkViewDescriptor();
+                        descriptor.Language = LanguageType.Javascript;
+                        descriptor.Templates.Add(sparkFile);
 
-                    File.WriteAllText(filePath, entry.SourceCode);
+                        //Maybe this has to be done in order so the other templates exist?
+                        var entry = factory.Engine.CreateEntry(descriptor);
+
+                        if (!Directory.Exists(Path.GetDirectoryName(jsFilePath)))
+                        {
+                            Directory.CreateDirectory(Path.GetDirectoryName(jsFilePath));
+                        }
+
+                        File.WriteAllText(jsFilePath, entry.SourceCode);
+                        filesProcessed++;
+                    }
                 }
+
+                Console.WriteLine("Processed {0} file(s)", filesProcessed);
             }
             catch (Exception exception)
             {
